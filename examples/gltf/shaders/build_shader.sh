ae47d9e2d760cb58dea8910be4c8ffb7ee9cb093
tonis <tanton@paysure.solutions>
2024-03-31 21:58:24 +0300

    Remove gltf example

--- examples/gltf/shaders/build_shader.sh
+++
@@ -1,5 +0,0 @@
-glslangValidator -V ./gbuffer.vert -o ./gbuffer_vert.spv
-glslangValidator -V ./gbuffer.frag -o ./gbuffer_frag.spv
-
-glslangValidator -V ./forward.vert -o ./forward_vert.spv
-glslangValidator -V ./forward.frag -o ./forward_frag.spv
