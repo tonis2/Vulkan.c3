14e5259a82ec9474183c8630b0091cd8df034235
tanton <tanton@paysure.solutions>
2024-09-05 21:44:12 +0300

    Update builder

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 curl https://raw.githubusercontent.com/KhronosGroup/Vulkan-Docs/main/xml/vk.xml --output ./assets/vk.xml
-dart run main.dart
+c3c run build
 cp ./build/*.c3 .
 zip ./vulkan.c3l ./*.c3 ./manifest.json
 rm ./*.c3
